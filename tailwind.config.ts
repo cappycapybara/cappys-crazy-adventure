a205ba074b5c84528539be092bd9cf7ddf26fb3c
Azza-Aaron <107457863+Azza-Aaron@users.noreply.github.com>
2024-03-16 16:40:17 +0800

    Cappy GO GO

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class", // or 'media' if you want media query based dark mode
+  purge: ["./src/**/*.tsx"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +15,52 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      typography: (theme: any) => ({
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            '[class~="lead"]': {
+              color: theme("colors.gray.400"),
+            },
+            blockquote: {
+              color: theme("colors.gray.300"),
+              borderLeftColor: theme("colors.gray.600"),
+            },
+            hr: {
+              borderColor: theme("colors.gray.700"),
+            },
+            "ol > li::before": {
+              color: theme("colors.gray.500"),
+            },
+            "ul > li::before": {
+              backgroundColor: theme("colors.gray.500"),
+            },
+            strong: {
+              color: theme("colors.white"),
+            },
+            thead: {
+              color: theme("colors.white"),
+              borderBottomColor: theme("colors.gray.400"),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      typography: ["dark"], // this line is important to extend dark mode
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
 };
+
 export default config;
